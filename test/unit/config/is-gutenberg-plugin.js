4ce298d1e404a1841c6ede5fecb576bde5ad4683
Adam Zieliński <adam@adamziel.com>
2022-04-29 12:15:05 +0200

    Simplify the boolean conversion of IS_GUTENBERG_PLUGIN

--- test/unit/config/is-gutenberg-plugin.js
+++ test/unit/config/is-gutenberg-plugin.js
@@ -1,34 +1,25 @@
-/*
-Package.json defines IS_GUTENBERG_PLUGIN as follows:
-
-	"config": {
-		"IS_GUTENBERG_PLUGIN": true
-	}
-
-Unfortunately, in here, it gets stringified like this:
-
-  "true" === process.env.npm_package_config_IS_GUTENBERG_PLUGIN
-
-Webpack replaces the occurrences of IS_GUTENBERG_PLUGIN with the boolean
-value, and the unit tests need to do the same.
-
-This code below converts the string value to its boolean counterpart,
-or preserves the original string in case it contains some unexpected
-value.
-*/
-const isPluginRaw = process.env.npm_package_config_IS_GUTENBERG_PLUGIN;
-let isPlugin;
-if ( isPluginRaw === 'true' ) {
-	isPlugin = true;
-} else if ( isPluginRaw === 'false' ) {
-	isPlugin = false;
-} else {
-	isPlugin = isPluginRaw;
-}
-
 global.process.env = {
 	...global.process.env,
-	// Inject the `IS_GUTENBERG_PLUGIN` global, used for feature flagging.
+	/*
+	Inject the `IS_GUTENBERG_PLUGIN` global, used for feature flagging.
+
+	The conversion to boolean is required here. Why? Package.json defines
+	IS_GUTENBERG_PLUGIN as follows:
+
+		"config": {
+			"IS_GUTENBERG_PLUGIN": true
+		}
+
+	Webpack then replaces references to IS_GUTENBERG_PLUGIN with a literal value `true`.
+	The file you are reading right now, however, receives a string value "true":
+
+		"true" === process.env.npm_package_config_IS_GUTENBERG_PLUGIN
+
+	The code can only work consistently in both environments when the value of
+	IS_GUTENBERG_PLUGIN is consistent. For this reason, the line below turns the
+	string representation of IS_GUTENBERG_PLUGIN into a boolean value.
+	*/
 	// eslint-disable-next-line @wordpress/is-gutenberg-plugin
-	IS_GUTENBERG_PLUGIN: isPlugin,
+	IS_GUTENBERG_PLUGIN:
+		String( process.env.npm_package_config_IS_GUTENBERG_PLUGIN ) === 'true',
 };
